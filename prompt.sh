80bb8448fddd286ee464314497c9b54197ddedcb
Loran <jpalvarezloran@gmail.com>
2020-02-22 22:10:47 -0600

    Ya lee comandos linux

--- prompt.sh
+++ prompt.sh
@@ -5,5 +5,9 @@ RESET="\033[0m"
 BOLD="\033[1m"
 YELLOW="\033[38;5;11m"
 BLUE="\033[0;34m"
-read -p "$(echo  $BOLD$YELLOW"$USER:"$BOLD$BLUE"$PWD"$RESET"$ ")" variable 
-echo "$variable"
+
+while read -p "$(echo  $BOLD$YELLOW"$USER:"$BOLD$BLUE"$PWD"$RESET"$ ")" variable 
+do 
+   command $variable
+done
+
